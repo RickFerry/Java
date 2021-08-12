acb6eabf0d3d00e595d2f3eba756c9e2983ee4e8
Ricardo Ferreira Martins <60847653+RickFerry@users.noreply.github.com>
2021-08-12 12:21:21 -0300

    Projeto e-commerce com front e microserviços

---
+++ Spring/e-commerce/payment-api/src/test/java/com/ferry/ecommerce/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferry.ecommerce.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
